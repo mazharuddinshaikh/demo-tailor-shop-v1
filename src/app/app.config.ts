9cecd62a66a6083e9c1e5d2f9dc1e4a9b2ea811d
ShaikhMazharuddin <mazhar.s2k18@gmail.com>
2025-12-29 20:18:41 +0530

    added primeng component library

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,15 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withComponentInputBinding()), provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withComponentInputBinding()), provideHttpClient(),
+    providePrimeNG({
+      theme: {
+          preset: Aura
+      }
+  })
+  ]
 };
